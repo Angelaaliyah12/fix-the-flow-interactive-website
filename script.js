708c3868f6cd14700145977065cef3f8ffcea56d
Angelaaliyah12 <angel.blom@hva.nl>
2025-12-17 21:20:30 +0100

    #10 js hamburg menu

---
+++ script.js
@@ -0,0 +1,6 @@
+let hamMenu =document.querySelector ('.ham-menu');
+let offScreenMenu =document.querySelector('.off-screen-menu');
+hamMenu.addEventListener ('click', () =>{
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+});
